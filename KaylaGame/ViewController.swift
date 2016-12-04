4fc4b4e6b7bf6c3935157be143a514c0d3439510
kayschmitt <kaylaschmitt@Kaylas-MacBook-Pro-2.local>
2016-12-04 15:56:11 -0600

    Initial Commit

---
+++ KaylaGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KaylaGame
+//
+//  Created by Kayla Schmitt on 12/4/16.
+//  Copyright © 2016 Kayla Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
