43cc95c236cb6ee38ed4ee8febc976ac32ed0c44
kayschmitt <kaylaschmitt@Kaylas-MacBook-Pro-2.local>
2017-02-11 22:20:30 -0600

    add more stuff

--- KaylaGame/PlayerNamesController.swift
+++ KaylaGame/PlayerNamesController.swift
@@ -1,37 +1,57 @@
-//
-//  PlayerNamesController.swift
-//  KaylaGame
-//
-//  Created by Kayla Schmitt on 2/11/17.
-//  Copyright © 2017 Kayla Schmitt. All rights reserved.
-//
-
 import UIKit
 
 class PlayerNamesController: UIViewController {
+
+    @IBOutlet weak var player1Label: UITextField!
+    @IBOutlet weak var player2Label: UITextField!
+    @IBOutlet weak var player3Label: UITextField!
+    @IBOutlet weak var player4Label: UITextField!
+    @IBOutlet weak var player5Label: UITextField!
+    @IBOutlet weak var player6Label: UITextField!
+    @IBOutlet weak var player7Label: UITextField!
+    @IBOutlet weak var player8Label: UITextField!
+    @IBOutlet weak var player9Label: UITextField!
+    @IBOutlet weak var player10Label: UITextField!
+
+    var playerLabels: [UITextField] = []
+    var activePlayerLabels: [UITextField] = []
 
     @IBOutlet weak var playerNumText: UILabel!
     var numPlayers: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        playerNumText.text = String(numPlayers)
 
-        // Do any additional setup after loading the view.
+        playerLabels = [
+            player1Label, player2Label, player3Label, player4Label, player5Label,
+            player6Label, player7Label, player8Label, player9Label, player10Label,
+        ]
+        activePlayerLabels = [player1Label, player2Label]
+        if numPlayers > 2 {
+            for i in 2 ... numPlayers - 1 {
+                playerLabels[i].isHidden = false
+                playerLabels.append(playerLabels[i])
+            }
+        }
+    }
+
+    @IBAction func submitButtonPressed(_: AnyObject) {
+        var fieldsFilled: Bool! = true
+        for label in activePlayerLabels {
+            if label.text == "" {
+                fieldsFilled = false
+            }
+        }
+        if !fieldsFilled {
+            let alert = UIAlertController(title: "Error", message: "Please enter all names!", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            // do stuff
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
 }
--- KaylaGame/ViewController.swift
+++ KaylaGame/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  KaylaGame
-//
-//  Created by Kayla Schmitt on 12/4/16.
-//  Copyright © 2016 Kayla Schmitt. All rights reserved.
-//
-
 import UIKit
 
 struct Player {
@@ -18,19 +10,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerNum: UILabel!
 
     @IBOutlet weak var newL: UILabel!
-    var allPlayers: [Player] = []
     var numPlayers: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        allPlayers.append(Player(name: "Kayla", score: 0))
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
