c6b84bb9b637d840f5aab2344fda87637bc147ec
kayschmitt <kaylaschmitt@Kaylas-MacBook-Pro-2.local>
2017-02-11 21:16:50 -0600

    Update Swift Formatting

--- KaylaGame/EnterPlayersController.swift
+++ KaylaGame/EnterPlayersController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class EnterPlayersController: UIViewController {
 
+    var mainView = ViewController()
+
+    @IBOutlet weak var playersNum: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        playersNum.text = String(mainView.numPlayers)
 
         // Do any additional setup after loading the view.
     }
@@ -20,16 +24,14 @@ class EnterPlayersController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
--- KaylaGame/ViewController.swift
+++ KaylaGame/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
+struct Player {
+    var name = ""
+    var score = 0
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var playerNum: UILabel!
+
+    @IBOutlet weak var newL: UILabel!
+    var allPlayers: [Player] = []
+    var numPlayers: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        allPlayers.append(Player(name: "Kayla", score: 0))
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +33,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        playerNum.text = Int(sender.value).description
+        numPlayers = Int(sender.value)
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
+        if segue.identifier == "segue_PlayerNamesController" {
+            let vc = segue.destination as! PlayerNamesController
+            vc.numPlayers = numPlayers
+        }
+    }
 }
-
