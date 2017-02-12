5b702d10bd8c3f86e0ab788bb4f09493f706f205
kayschmitt <kaylaschmitt@Kaylas-MacBook-Pro-2.local>
2017-02-11 21:10:15 -0600

    add readme

---
+++ KaylaGame/PlayerNamesController.swift
@@ -0,0 +1,35 @@
+//
+//  PlayerNamesController.swift
+//  KaylaGame
+//
+//  Created by Kayla Schmitt on 2/11/17.
+//  Copyright © 2017 Kayla Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class PlayerNamesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# kayla-drinking-game
